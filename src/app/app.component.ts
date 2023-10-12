9d543d05f2f869a44b85154f35850044701ba80d
Росса Артём Дмитриевич <rossa@sc.softclub.by>
2023-10-12 09:30:07 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  imports: [RouterOutlet],
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'priloga';
+}
