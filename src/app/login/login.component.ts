c4e66dc5b1722f87255f94213b6db23b97584990
Росса Артём Дмитриевич <rossa@sc.softclub.by>
2023-10-13 12:52:55 +0300

    Add FormGroup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Route, Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+  ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  constructor( private readonly router: Router){
 
+  public formLog = new FormGroup({
+    login: new FormControl(),
+    password: new FormControl()
+  })
+
+
+  constructor(private readonly router: Router){
+    
   }
   public openRegister(): void {
     this.router.navigateByUrl("register");
+    
   }
   public openActivity(): void {
     this.router.navigateByUrl("activ");
   }
+
+  public contrLogin(): void {
+    console.log(this.formLog.value)
+  }
+
 }
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,19 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+  ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent {
-  constructor( private readonly router: Router){
 
-  }
+  public formReg = new FormGroup({
+    login: new FormControl(),
+    password: new FormControl(),
+    confirmPassword: new FormControl(),
+    initials: new FormControl(),
+    email: new FormControl()
+  })
+
+  constructor( private readonly router: Router){  }
   public openLogin(): void {
     this.router.navigateByUrl("login");
   }
+
+  public contrRegistration(): void{
+    console.log(this.formReg.value)
+  }
+
 }
